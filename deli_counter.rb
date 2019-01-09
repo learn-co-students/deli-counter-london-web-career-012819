521066e51d5dce1d7a94389186463fdd0f92c8c5
Kian Edalat <akedalat@gmail.com>
2019-01-09 16:50:50 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  elsif array.size > 0
+    i = 0
+    sentence = "The line is currently:"
+    while i < array.size
+      sentence += " #{i+1}. #{array[i]}"
+      i += 1
+      end
+
+puts sentence
+  end
+end
